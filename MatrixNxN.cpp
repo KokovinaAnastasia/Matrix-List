243d2cdb7e8f015d523c85d81b8b6de95460a788
KokovinaAnastasia <65601284+KokovinaAnastasia@users.noreply.github.com>
2020-05-19 16:32:52 +0500

    Add files via upload

---
+++ MatrixNxN.cpp
@@ -0,0 +1,89 @@
+#include "pch.h"
+#include "MatrixNxN.h"
+#include <cstdlib>
+
+using namespace std;
+
+MatrixNxN::MatrixNxN(const int dimension)
+{
+	arr = new int*[dimension];
+	for (int i = 0; i < dimension; i++)
+		arr[i] = new int[dimension];
+	size = dimension;
+}
+
+int MatrixNxN::element(const int i, const int j) const
+{
+	return arr[i][j];
+};
+
+void MatrixNxN::setElement(const int i, const int j, const int value)
+{
+	arr[i][j] = value;
+};
+
+void MatrixNxN::fillRandomElements(const int minVal, const int maxVal)
+{
+	for (int i = 0; i < size; i++)
+		for (int j = 0; j < size; j++)
+			setElement(i, j, (rand() % (maxVal - minVal)) + minVal);
+};
+
+int  MatrixNxN::sumPrincipalDiag() const
+{
+	int sum = 0;
+	for (int i = 0; i < size; i++)
+		sum += element(i, i);
+	return sum;
+};
+
+int MatrixNxN::sumSecondaryDiag() const
+{
+	int sum = 0;
+	for (int i = 0; i < size; i++)
+		sum += element(size - i - 1, i);
+	return sum;
+};
+
+int MatrixNxN::productPrinsipalDiag() const
+{
+	int prod = 1;
+	for (int i = 0; i < size; i++)
+		prod *= element(i, i);
+	return prod;
+};
+
+int MatrixNxN::productSecondaryDiag() const
+{
+	int prod = 1;
+	for (int i = 0; i < size; i++)
+		prod *= element(size - i - 1, i);
+	return prod;
+};
+int  MatrixNxN::sumRow(const int iRow) const
+{
+	int sum = 0;
+	for (int i = 0; i < size; i++)
+		sum += element(iRow, i);
+	return sum;
+};
+
+int MatrixNxN::minColumn(const int iCol) const
+{
+	int min = element(0, iCol);
+	for (int i = 1; i < size; i++)
+	{
+		if (element(i, iCol) < min) min = element(i, iCol);
+	}
+	return min;
+};
+
+int MatrixNxN::maxColumn(const int iCol) const
+{
+	int max = element(0, iCol);
+	for (int i = 1; i < size; i++)
+	{
+		if (element(i, iCol) > max) max = element(i, iCol);
+	}
+	return max;
+};
---
+++ lab6.2.cpp
@@ -0,0 +1,32 @@
+﻿#include "pch.h"
+#include <iostream>
+#include "MatrixNxN.h"
+
+using namespace std;
+
+int main()
+{
+	MatrixNxN matr = MatrixNxN(4);
+	matr.fillRandomElements(-10, 10);
+
+	for (int i = 0; i < matr.size; i++)
+	{
+		for (int j = 0; j < matr.size; j++)
+			cout << matr.element(i, j) << "\t";
+		cout << endl;
+	};
+	cout << endl;
+	cout << "SumPrincipalDiagonal = " << matr.sumPrincipalDiag() << endl;
+	cout << "SumSideDiagonal = " << matr.sumSecondaryDiag() << endl;
+	cout << "ProdPrincipalDiagonal = " << matr.productPrinsipalDiag() << endl;
+	cout << "ProdSideDiagonal = " << matr.productSecondaryDiag() << endl << endl;
+	
+	for (int i = 0; i < matr.size; i++)
+	{
+		cout << "SumRow" << i + 1 << "= " << matr.sumRow(i) << endl;
+		cout << "MaxInColumn" << i + 1 << " = " << matr.maxColumn(i) << endl;
+		cout << "MinInColumn" << i + 1 << " = " << matr.minColumn(i) << endl << endl;
+	}
+	system("pause");
+	return 0;
+}
